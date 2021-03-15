ee16909add3890ba90512b6150aecdfca802adf4
Eugene <45402182+Evgen1188@users.noreply.github.com>
2021-03-15 18:40:50 +0300

    Update config.py
    application factory

--- config.py
+++ config.py
@@ -1,2 +1,11 @@
-SQLALCHEMY_DATABASE_URI = "postgresql:///book_store"
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+class Config:
+    SQLALCHEMY_DATABASE_URI = "postgresql:///book_store"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class ProductionConfig(Config):
+    pass
+class DevelopmentConfig(Config):
+    pass
+  
+ class TestConfig(Config):
+    TESTING = True
